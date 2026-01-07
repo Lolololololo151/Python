dacaf8ce46aec302b8a0ba017d3792ae289986b2
Roma <>
2026-01-07 14:34:29 +0200

    Before going to work on multiplayer, i have improved checkers, now its more "modular", and choose a team randomly at the start of the game

--- checkers.py
+++ checkers.py
@@ -15,29 +15,39 @@ BLACK_CHECKER = pygame.image.load(os.path.join('black.png'))
 WHITE_CHECKER = pygame.image.load(os.path.join('white.png'))
 ARE_YOU_BLACK_OR_WHITE = random.randint(0, 1)
 
-if ARE_YOU_BLACK_OR_WHITE == 0:
-    pygame.display.set_caption("Checkers white team")
-elif ARE_YOU_BLACK_OR_WHITE == 1:
-    pygame.display.set_caption("Checkers black team")
-else:
-    pygame.display.set_caption("Checkers")
+SCREEN.fill("brown")
 
-for row in range(8):
-    for col in range(8):
-        x = 100 + col * CELL_WIDTH
-        y = 50 + row * CELL_HEIGHT
-        BOARD_CELLS.append(pygame.Rect(x, y, CELL_WIDTH, CELL_HEIGHT))
+def team_Chooser():
+    if ARE_YOU_BLACK_OR_WHITE == 0:
+        pygame.display.set_caption("Checkers white team")
+    elif ARE_YOU_BLACK_OR_WHITE == 1:
+        pygame.display.set_caption("Checkers black team")
+    else:
+        pygame.display.set_caption("Checkers")
 
-while RUNNING:
-    CurrentPressedKey = pygame.key.get_pressed()
+team_Chooser()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
-    
-    SCREEN.fill("brown")
-    pygame.draw.rect(surface=SCREEN, color=(0, 0, 0), rect=BOARD, width=2)
+def prepareCells():
+    for row in range(8):
+        for col in range(8):
+            x = 100 + col * CELL_WIDTH
+            y = 50 + row * CELL_HEIGHT
+            BOARD_CELLS.append(pygame.Rect(x, y, CELL_WIDTH, CELL_HEIGHT))
 
+prepareCells()
+
+def drawBoard():
+    pygame.draw.rect(surface=SCREEN, color=(0, 0, 0), rect=BOARD, width=10)
+
+drawBoard()
+
+def drawCells():
+    for cell in BOARD_CELLS:
+        pygame.draw.rect(surface=SCREEN, color=(0, 0, 0), rect=cell, width=5)
+
+drawCells()
+
+def drawCheckers():
     for i in range(24):
         cell = BOARD_CELLS[i]
         SCREEN.blit(WHITE_CHECKER, (cell.x, cell.y))
@@ -46,7 +56,11 @@ while RUNNING:
         cell = BOARD_CELLS[i]
         SCREEN.blit(BLACK_CHECKER, (cell.x, cell.y))
 
-    for cell in BOARD_CELLS:
-        pygame.draw.rect(surface=SCREEN, color=(0, 0, 0), rect=cell, width=5)
+drawCheckers()
+
+while RUNNING:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
     
     pygame.display.flip()
